c9d601d4464e652495af2c96de4a9ba9cd605738
Jo-Nakagami <jn813mo@icloud.com>
2025-09-02 17:10:31 +0900

    編集中のタスクの編集ボタンを赤色に変更した

--- server/app/_components/TodoItem.tsx
+++ server/app/_components/TodoItem.tsx
@@ -1,4 +1,4 @@
-import { FaCheckCircle } from "react-icons/fa";
+import { FaCheckCircle, FaPen } from "react-icons/fa";
 import { TodoData, TodoStatus } from "@/app/_types/TodoTypes";
 
 type TodoItemProps = {
@@ -32,7 +32,23 @@ const TodoItem = ({ todo, onEditBeginingHandler }: TodoItemProps): JSX.Element =
       itemDesign.bgColor = "bg-emerald-500";
       break;
   }
+ // 編集中の状態に追加するスタイル
+  const editingStyles = isediting ? "border-2 border-red-500 bg-red-50" : "";
 
+  // 編集中の「編集」ボタン用のスタイル
+  const editButtonStyles = isediting
+    ? "bg-red-600 hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600 transition-colors duration-300"
+    : "bg-indigo-600 hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transition-colors duration-300";
+
+  // ボタンをクリックしたときに isediting の状態を切り替える
+  const [isEditing, setIsEditing] = useState<boolean>(isediting);
+
+  const handleEditClick = () => {
+    if (onEditBeginingHandler) {
+      onEditBeginingHandler(todo);
+    }
+    setIsEditing(!isEditing);
+  };
   return (
     <div className="flex w-full border border-gray-300 max-w-sm overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800">
       <div className={`flex items-center justify-center w-12 ${itemDesign.bgColor}`}>
@@ -50,10 +66,12 @@ const TodoItem = ({ todo, onEditBeginingHandler }: TodoItemProps): JSX.Element =
           <p className="text-sm text-gray-600 dark:text-gray-200">
             {todo.description}
           </p>
+          {/* 編集ボタン */}
           <button
-            className="flex w-15 justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            onClick={() => onEditBeginingHandler(todo)}
+            className={`flex w-20 justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm ${editButtonStyles}`}
+            onClick={handleEditClick}
           >
+            <FaPen className="mr-2" /> {/*FaPen アイコンをボタン内に追加*/}
             編集
           </button>
         </div>
