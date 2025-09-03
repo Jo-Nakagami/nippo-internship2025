0a53fb2cd9e8d41a02065067bbed98f3007c09c4
Jo-Nakagami <jn813mo@icloud.com>
2025-09-03 11:04:46 +0900

    Update server/app/page.tsx
    Co-authored-by: Akihito Horie <79936171+akisan1025@users.noreply.github.com>

--- server/app/page.tsx
+++ server/app/page.tsx
@@ -35,7 +35,8 @@ export default async function Home() {
     <>
       <h1 className="text-4xl font-bold text-black-400">
         <span className="text-red-500">To</span>
-        <span className="text-blue-500">Do</span>リスト
+        <span className="text-blue-500">Do</span>
+        リスト
       </h1>
       <TodoForm children={data} />
     </>
